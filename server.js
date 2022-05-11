1267f69376cdf613c5ef382b96c04a4e8b6d5304
Colt <98503459+coltschultz@users.noreply.github.com>
2022-05-10 19:49:17 -0500

    remove files after used

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ http
             res.end();
           });
           getEntirePage(newpath);
-          fs.unlink(newpath);
+          fs.unlinkSync(newpath);
         });
     } else {
       res.writeHead(200, { "Content-Type": "text/html" });
